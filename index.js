b971926efdaf8ce5ef14a728085b3252cd4538eb
Niraj <nirajp.tt.19@nitj.ac.in>
2023-08-18 15:15:36 +0530

    first commmit

---
+++ index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+//mongoose connect
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+  })
+  .then(console.log("Connected to MongoDb"))
+  .catch((err) => {
+    console.log("connecting problem Error: ", err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is up and running");
+});
