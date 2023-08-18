ea60a4a4fcd29e2ab720b18f28e99e9d807f082c
Niraj <nirajp.tt.19@nitj.ac.in>
2023-08-19 00:51:05 +0530

    index modifie

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const dotenv = require("dotenv");
-const app = express();
+const userRouter = require("./routers/user");
+const adminRouter = require("./routers/admin");
 dotenv.config();
-app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URL, {
@@ -13,6 +13,11 @@ mongoose
   .catch((err) => {
     console.log("connecting problem Error: ", err);
   });
+
+const app = express();
+app.use(express.json());
+app.use("/api/users", userRouter);
+app.use("/api/admin", adminRouter);
 
 app.listen(3000, () => {
   console.log("Server is up and running on post : ", 3000);
