3524028c161b57490473db577a52bc478128b9f2
Niraj <nirajp.tt.19@nitj.ac.in>
2023-08-23 23:18:31 +0530

    minor change to auth middleware

--- middleware/auth_dealership.js
+++ middleware/auth_dealership.js
@@ -12,6 +12,7 @@ const auth = async (req, res, next) => {
     if (!dealership) {
       throw new Error();
     }
+    req.token = token;
     req.dealership = dealership;
     next();
   } catch (error) {
